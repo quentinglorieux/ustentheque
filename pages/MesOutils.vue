5151a82dc2b6c27e93ea8b5be5dc7435d4809dc9
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-04-21 06:48:30 +0200

    update AppMenu.vue, MesOutils.vue, Outil-[id].vue and Login.vue

--- pages/MesOutils.vue
+++ pages/MesOutils.vue
@@ -3,8 +3,29 @@
         <div className="col-12">
             <div className="card">
                 <h5>Mes outils</h5>
+                {{ me`` }}
                 <p>Use this page to start from scratch and place your custom content.</p>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+import { Directus } from "@directus/sdk";
+
+const directus = new Directus("https://devdirectus.rubidiumweb.eu");
+
+const me = ref('')
+
+async function myProfile() {
+    me.value = await directus.users.me.read();
+}
+
+onMounted(() => {
+    myProfile();
+});
+
+
+
+
+</script>
