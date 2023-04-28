f24de95a3898e1557d213a0f4c40ea4134d4e5e7
Abigaël Laldji <ablaldji@gmail.com>
2023-04-28 16:01:36 +0200

    feat(resa) request button add resa in db

--- pages/Outil-[id].vue
+++ pages/Outil-[id].vue
@@ -1,46 +1,82 @@
 <template>
-  <div v-if="objet" class="flex flex-column gap-1 ">
+  <div v-if="objet" class="flex flex-column gap-1">
     <span class="text-2xl"> {{ objet.nom }}</span>
     <img
-      class="w-40 h-40 sm:w-16rem sm:h-16rem xl:w-10rem xl:h-10rem object-contain block xl:block  border-round"
+      class="w-40 h-40 sm:w-16rem sm:h-16rem xl:w-10rem xl:h-10rem object-contain block xl:block border-round"
       :src="`https://devdirectus.rubidiumweb.eu/assets/${objet.photo}?quality=50`"
     />
-   
   </div>
-  <div  class="flex content-center gap-2"> 
-        <div class="mt-1" > Propiétaire: </div>
-      <Chip class="mb-1 bg-slate-50" :label="proprio.first_name + ' ' + proprio.last_name" :image="`https://devdirectus.rubidiumweb.eu/assets/${proprio.avatar}?fit=cover&width=50&height=50&quality=20`" />
-      </div>
- 
+  <div class="flex content-center gap-2">
+    <div class="mt-1">Propiétaire:</div>
+    <Chip
+      class="mb-1 bg-slate-50"
+      :label="proprio.first_name + ' ' + proprio.last_name"
+      :image="`https://devdirectus.rubidiumweb.eu/assets/${proprio.avatar}?fit=cover&width=50&height=50&quality=20`"
+    />
+  </div>
 
-  <div class="mb-10">  
-    <div class="mb-2"> <Calendar showIcon v-model="dates" selectionMode="range" :manualInput="false" /> </div>
-     <Button v-if="!dates" disabled label="Envoyer une demande de reservation" severity="info" class="mr-2 mb-2 "></Button> 
-     <Button v-if="dates"  label="Envoyer une demande de reservation" severity="info" class="mr-2 mb-2 "></Button> </div>
- 
+  <div class="mb-10">
+    <div class="mb-2">
+      <Calendar
+        showIcon
+        v-model="dates"
+        selectionMode="range"
+        :manualInput="false"
+      />
+    </div>
+    <Button
+      v-if="!dates"
+      disabled
+      label="Envoyer une demande de reservation"
+      severity="info"
+      class="mr-2 mb-2"
+    ></Button>
+    <Button
+      v-if="dates"
+      label="Envoyer une demande de reservation"
+      severity="info"
+      class="mr-2 mb-2"
+      @click="requestResa"
+    ></Button>
+  </div>
 
-     <NuxtLink to="/catalogue"> <Button label="Catalogue" class="mr-2 mb-2 w-1"></Button></NuxtLink>
-
+  <NuxtLink to="/catalogue">
+    <Button label="Catalogue" class="mr-2 mb-2 w-1"></Button
+  ></NuxtLink>
 </template>
 
 <script setup>
 import { Directus } from "@directus/sdk";
+import { useToast } from "primevue/toast";
 const route = useRoute();
+const toast = useToast();
 
 const directus = new Directus("https://devdirectus.rubidiumweb.eu");
 
 const objet = ref("");
-const dates =ref('')
-const proprio =ref('')
+const dates = ref("");
+const proprio = ref("");
 
 async function retrieveOneObjet() {
-  const publicData = await directus.items("objet").readOne(route.params.id, 
-  {fields: [
-          "*,proprietaire.*"
-        ]
-  });
+  const publicData = await directus
+    .items("objet")
+    .readOne(route.params.id, { fields: ["*,proprietaire.*"] });
   objet.value = publicData;
-  proprio.value = publicData.proprietaire ;
+  proprio.value = publicData.proprietaire;
+}
+
+async function requestResa() {
+  const resa = await directus.items("reservation").createOne({
+    objet: route.params.id,
+    debut: dates.value[0],
+    fin: dates.value[1],
+    statut: "En attente",
+  });
+  if (resa.id) {
+    toast("Votre demande de réservation a bien été envoyée");
+  }else {
+    toast("Votre demande de réservation n'a pas pu être envoyée");
+  }
 }
 
 onMounted(() => {
