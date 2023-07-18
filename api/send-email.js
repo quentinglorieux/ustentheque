e38384f3d8261594f2ced14c35e06407b31d9f19
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-07-18 13:30:49 +0200

    update send-email.js and vercel.json

--- api/send-email.js
+++ api/send-email.js
@@ -3,7 +3,7 @@ const sgMail = require("@sendgrid/mail");
 export default async function (req, res) {
   try {
     console.log('hello')
-    console.log(process.env.SENDGRID_API_KEY)
+
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     const msg = {
