060cdfd772b1757e99342e7417f249a41c718bf1
Quentin Glorieux <quentin.glorieux@gmail.com>
2025-11-20 17:36:28 +0100

    update AppTopbar.vue

--- components/app/AppTopbar.vue
+++ components/app/AppTopbar.vue
@@ -68,7 +68,7 @@ const store = useAuthStore();
 const first_name = computed(() => store.first_name);
 const avatar = computed(() => store.avatar);
 
-const version = "1.1";
+const version = "1.2";
 
 const outsideClickListener = ref(null);
 const topbarMenuActive = ref(false);
