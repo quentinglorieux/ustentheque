d47c7ba44a56454cb288c64ef2cf29517be2b9b6
Quentin Glorieux <quentin.glorieux@gmail.com>
2025-11-20 17:28:25 +0100

    build(deps): update package-lock.json and directusConfig.ts

--- utils/directusConfig.ts
+++ utils/directusConfig.ts
@@ -1,8 +1,7 @@
-const envBaseFromProcess =
-  typeof process !== "undefined" ? process.env?.NUXT_PUBLIC_DIRECTUS_BASE : undefined;
-const envBaseFromImportMeta =
-  typeof import.meta !== "undefined" ? import.meta.env?.NUXT_PUBLIC_DIRECTUS_BASE : undefined;
+const directusFromProcess =
+  typeof process !== "undefined"
+    ? process.env?.NUXT_PUBLIC_DIRECTUS_BASE ?? process.env?.DIRECTUS_BASE
+    : undefined;
 
 export const DIRECTUS_BASE =
-  envBaseFromProcess ?? envBaseFromImportMeta ??
-  "https://devdirectus.rubidiumweb.eu";
+  directusFromProcess ?? "https://devdirectus.rubidiumweb.eu";
